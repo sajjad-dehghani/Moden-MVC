c32d15f356d78fdeaabef382976d884d8ab2534d
Sajjad Dehghani <s_dehghani@modernisc.com>
2017-06-21 11:49:10 +0430

    init project files

---
+++ ModernMVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ModernMVCProject.Startup))]
+namespace ModernMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
